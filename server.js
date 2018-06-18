67fabcfd02ab5ce33471988f4494d39d3e7210b7
kat thumma <katthumma@gmail.com>
2018-06-18 16:13:19 -0400

    third try heroku

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.set('view engine','handlebars');
 var routes = require('./controllers/suggestions_controller.js');
 app.use('/',routes);
 
-var port = 3000;
+var port = process.env.port || 3000;
 app.listen(port);
